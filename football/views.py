d6260563252e1682bb78e9c0108f580e71fb5053
xHacker404 <sulaiman.haque@gmail.com>
2019-03-09 21:46:53 +0600

    date_format done

--- football/views.py
+++ football/views.py
@@ -72,15 +72,17 @@ def get_match_data(request):
 def date_format(date):
 
     temp_date = date.split('-')
-    day = int(temp_date [2])
+
+    year = int(temp_date[0])
     month = int(temp_date [1])
+    day = int(temp_date [2])
 
     if day == datetime.datetime.now().date().day :
         return "Today"
     elif is_next_day(day):
          return "Tommorow"
     else:
-        return temp_date # have to work here
+        return weekDay(year,month,day) + "," +str(month) +"/"+ str(day) # have to work here
 
 # Converting json time string to custom time format
 def time_format(time):
@@ -137,3 +139,27 @@ def is_leap_year(year):
                 return True;
     else:
         return False
+
+# Find day of week
+def weekDay(year, month, day):
+    offset = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+    week   = ['Sun',
+              'Mon',
+              'Tues',
+              'Wed',
+              'Thurs',
+              'Fri',
+              'Sat']
+    afterFeb = 1
+    if month > 2: afterFeb = 0
+    aux = year - 1700 - afterFeb
+    # dayOfWeek for 1700/1/1 = 5, Friday
+    dayOfWeek  = 5
+    # partial sum of days betweem current date and 1700/1/1
+    dayOfWeek += (aux + afterFeb) * 365
+    # leap year correction
+    dayOfWeek += aux / 4 - aux / 100 + (aux + 100) / 400
+    # sum monthly and day offsets
+    dayOfWeek += offset[month - 1] + (day - 1)
+    dayOfWeek %= 7
+    return week[int(dayOfWeek)]
